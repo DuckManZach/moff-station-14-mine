c4a71f39f46eeafbf35e304e7940d5aee7832735
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-01-06 10:08:29 -0700

    Icon and stuff

---
+++ Content.Shared/_Harmony/BloodBrothers/Components/BloodBrotherConvertableComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._Harmony.BloodBrothers.Components;
+
+/// <summary>
+/// This is used for tracking who is eligible to be converted to a blood brother based on their character preference
+/// </summary>
+[RegisterComponent]
+public sealed partial class BloodBrotherConvertableComponent : Component;
